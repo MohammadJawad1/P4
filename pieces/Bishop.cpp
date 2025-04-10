f581c09b29f4bf84b7b6c8cda2e0dcc6b1d8a481
MohammmadJawad1 <mohammad.jawad30@myhunter.cuny.edu>
2025-04-09 22:46:49 -0400

    fixed the double ;

--- pieces/Bishop.cpp
+++ pieces/Bishop.cpp
@@ -17,6 +17,26 @@ Bishop::Bishop(const std::string& color, const int& row, const int& col, const b
     : ChessPiece(color, row, col, movingUp, 3, "BISHOP") {}
 
 // YOUR CODE HERE
+/**
+ * @brief Determines whether the Bishop can move to the specified target position on the board.
+ * @pre The row & col members of the Bishop object match its actual position on the passed board object.
+ * 
+ * The Bishop's movement follows these rules:
+ * 1. The Bishop may move any number of squares diagonally from its current position
+ * 2. The path to the target square must be unobstructed by (ie. contain) other pieces, 
+ *      except for the target square itself.
+ * 3. The target square must either be empty (moving) or contain a piece of another color (capturing)
+ * 4. The target square must stay within the board's bounds, ie. not outside [0, BOARD_LENGTH)
+ * 5. The Bishop cannot move to its currently occupied position (i.e. it can't stand still).
+ * 
+ * @param target_row An integer representing the row index of the target square on the board.
+ * @param target_col An integer representing the column index of the target square on the board.
+ * @param board A 2D vector representing the current board state, 
+ *              where each cell points to a ChessPiece object or is null.
+ * 
+ * @return True if the Bishop can move to the specified position; false otherwise.
+ */
+
 bool Bishop::canMove(const int& target_row, const int& target_col, const std::vector<std::vector<ChessPiece*>>& board) const 
 {
     // Not on the board 
@@ -34,7 +54,7 @@ bool Bishop::canMove(const int& target_row, const int& target_col, const std::ve
 
     // Same cell OR not a diagonal line
     bool stays_in_same_position = (row_difference == 0) && (col_difference == 0);
-    bool moves_diagonal = (std::abs(row_difference) == std::abs(col_difference));;
+    bool moves_diagonal = (std::abs(row_difference) == std::abs(col_difference));
     if (stays_in_same_position || !moves_diagonal) { return false; }
 
     // Find what direction we should step
