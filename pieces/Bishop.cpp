3f1905a2804d07f6cebae2137a604ffd082746dd
MohammmadJawad1 <mohammad.jawad30@myhunter.cuny.edu>
2025-04-09 22:00:05 -0400

    updated bishop movement

--- pieces/Bishop.cpp
+++ pieces/Bishop.cpp
@@ -27,5 +27,34 @@ bool Bishop::canMove(const int& target_row, const int& target_col, const std::ve
     ChessPiece* target_piece = board[target_row][target_col];
     if (target_piece && target_piece->getColor() == getColor()) { return false; }
 
-    return false;
+    // Get the difference between the current position and the target position
+    int row_difference = target_row - getRow(); 
+    // -,0,+  -->  represents down, no movement, up
+    int col_difference = target_col - getColumn(); 
+
+    // Same cell OR not a diagonal line
+    bool stays_in_same_position = (row_difference == 0) && (col_difference == 0);
+    bool moves_diagonal = (row_difference == col_difference);
+    if (stays_in_same_position || !moves_diagonal) { return false; }
+
+    // Find what direction we should step
+    int increment_row = 0;
+    int increment_col = 0;
+    if (row_difference > 0) { increment_row = 1; }  // Moving up
+    if (row_difference < 0) { increment_row = -1; } // Moving down
+
+    if (col_difference > 0) { increment_col = 1; }  // Moving right
+    if (col_difference < 0) { increment_col = -1; } // Moving left
+
+    // Iterate from the original space to target space and check if there is any obstructing Chess Piece
+    int temp_row = getRow();
+    int temp_col = getColumn();
+
+    while (temp_row != target_row || temp_col != target_col) {
+        temp_row += increment_row;
+        temp_col += increment_col;
+        if (board[temp_row][temp_col]) { return false; }
+    }
+
+    return true;
 }
