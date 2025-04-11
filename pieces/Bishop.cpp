28b8f3f1949b1cab4a2d1aaa4587241077f11686
MohammmadJawad1 <mohammad.jawad30@myhunter.cuny.edu>
2025-04-11 15:43:16 -0400

    changes to while loop

--- pieces/Bishop.cpp
+++ pieces/Bishop.cpp
@@ -44,11 +44,6 @@ bool Bishop::canMove(const int& target_row, const int& target_col, const std::ve
     // Out of bounds target
     if (target_row < 0 || target_row >= BOARD_LENGTH || target_col < 0 || target_col >= BOARD_LENGTH) { return false; };
 
-    ChessPiece* target_piece = board[target_row][target_col];
-    if (target_piece) {
-        if (target_piece->getColor() == getColor()) { return false; } 
-    }
-
     // Get the difference between the current position and the target position
     int row_difference = target_row - getRow(); 
     int col_difference = target_col - getColumn(); 
@@ -68,14 +63,16 @@ bool Bishop::canMove(const int& target_row, const int& target_col, const std::ve
     if (col_difference < 0) { increment_col = -1; } 
 
     // Iterate from the original space to target space and check if there is any obstructing Chess Piece
-    int temp_row = getRow();
-    int temp_col = getColumn();
+    int temp_row = getRow() + increment_row;
+    int temp_col = getColumn() + increment_col;
 
     while (temp_row != target_row || temp_col != target_col) {
+        if (board[temp_row][temp_col]) return false;
         temp_row += increment_row;
         temp_col += increment_col;
-        if (board[temp_row][temp_col]) { return false; }
     }
-
+    ChessPiece* target_piece = board[target_row][target_col];
     return (target_piece == nullptr || target_piece->getColor() != getColor());
+
+    return true;
 }
